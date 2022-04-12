c1356284112bab905fe580c2e31702e1f04ee0b1
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-04-12 12:44:26 -0700

    update footer text

--- pages/components/Footer/footer.js
+++ pages/components/Footer/footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 export default function Footer(){
     return (
         <footer className={styles.footer}>
-            <span className={styles.text}>Powered with ❤️ and <a href="">Node-Deployment-Server</a></span>
+            <span className={styles.text}>Powered with ❤️ and <a href="https://github.com/Joshua-Zou/node-deployment-server" rel="noreferrer" target="_blank">Node-Deployment-Server</a> |             <a href="https://github.com/Joshua-Zou/node-deployment-server#update-guide" rel="noreferrer" target="_blank">Update Guide</a></span>
         </footer>
     )
 }
