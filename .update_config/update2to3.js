ca38097ee6d5a3674eb7ecc4a9ab2f8ddcff9c79
Joshua-Zou <77520157+Joshua-Zou@users.noreply.github.com>
2022-04-12 17:59:02 -0700

    Update update2to3.js

--- .update_config/update2to3.js
+++ .update_config/update2to3.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
     if (config.deployments) {
         for (let i in config.deployments) {
             if (!config.deployments[i].environmentVariables) {
-                config.deployments[i].environmentVariables = [];
+                config.deployments[i].environmentVariables = {};
             }
         }
     }
